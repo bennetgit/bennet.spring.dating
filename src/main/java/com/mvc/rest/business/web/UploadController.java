bb0d9f99ef0ae8f1a6b93ab85d2d87b4fb658665
bennetgit <bennetchen22@gmail.com>
2012-11-04 23:52:15 +0800

    文件上传功能

---
+++ src/main/java/com/mvc/rest/business/web/UploadController.java
@@ -0,0 +1,115 @@
+package com.mvc.rest.business.web;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mvc.rest.beans.WebConfig;
+import com.mvc.uitls.MultipartUploadParser;
+import com.mvc.uitls.RequestParser;
+import com.mvc.uitls.SpringContextUtil;
+
+@Controller
+public class UploadController<K> {
+	private Logger logger = Logger.getLogger(UploadController.class.getName());
+
+	static WebConfig webConfig = (WebConfig) SpringContextUtil.getBean("webConfig");
+    private static File UPLOAD_DIR = new File((String) webConfig.getParams().get("imageUploadPath"));
+    private static File TEMP_DIR = new File("uploadsTemp");
+
+    private static String CONTENT_TYPE = "text/plain";
+    private static String CONTENT_LENGTH = "Content-Length";
+    private static int RESPONSE_CODE = 200;
+	
+	@ResponseBody  
+	@RequestMapping("/upload")
+	public Object upload(HttpServletRequest req, HttpServletResponse resp) {
+		
+		Map<String, Object> ret = new HashMap<String, Object>();
+		
+        String contentLengthHeader = req.getHeader(CONTENT_LENGTH);
+        Long expectedFileSize = StringUtils.isBlank(contentLengthHeader) ? null : Long.parseLong(contentLengthHeader);
+		
+        logger.info("--------- expectedFileSize " + expectedFileSize + "--------------");
+        
+        RequestParser requestParser;
+        
+        try
+        {
+            resp.setContentType(CONTENT_TYPE);
+            resp.setStatus(RESPONSE_CODE);
+
+            if (ServletFileUpload.isMultipartContent(req))
+            {
+                requestParser = RequestParser.getInstance(req, new MultipartUploadParser(req, TEMP_DIR, req.getServletContext()));
+                doWriteTempFileForPostRequest(requestParser);
+                ret.put("success", true);
+            }
+            else
+            {
+                requestParser = RequestParser.getInstance(req, null);
+                writeToTempFile(req.getInputStream(), new File(UPLOAD_DIR, requestParser.getFilename()), expectedFileSize);
+                ret.put("success", true);
+            }
+        } catch (Exception e)
+        {
+        	logger.error("Problem handling upload request", e);
+            ret.put("error", "test error");
+        }
+        
+		//ret.put("success", true);
+		//ret.put("error", "test error");
+		return ret;
+	}
+	
+    private void doWriteTempFileForPostRequest(RequestParser requestParser) throws Exception
+    {
+        writeToTempFile(requestParser.getUploadItem().getInputStream(), new File(UPLOAD_DIR, requestParser.getFilename()), null);
+    }
+
+    private File writeToTempFile(InputStream in, File out, Long expectedFileSize) throws IOException
+    {
+        FileOutputStream fos = null;
+
+        try
+        {
+            fos = new FileOutputStream(out);
+
+            IOUtils.copy(in, fos);
+
+            if (expectedFileSize != null)
+            {
+                Long bytesWrittenToDisk = out.length();
+                if (!expectedFileSize.equals(bytesWrittenToDisk))
+                {
+                	logger.warn(String.format("Expected file %s to be %s bytes; file on disk is %s bytes", out.getAbsolutePath(), expectedFileSize, 1));
+                    throw new IOException(String.format("Unexpected file size mismatch. Actual bytes %s. Expected bytes %s.", bytesWrittenToDisk, expectedFileSize));
+                }
+            }
+
+            return out;
+        }
+        catch (Exception e)
+        {
+            throw new IOException(e);
+        }
+        finally
+        {
+            IOUtils.closeQuietly(fos);
+        }
+    }	
+}
--- src/main/webapp/WEB-INF/view/photoupload.jsp
+++ src/main/webapp/WEB-INF/view/photoupload.jsp
@@ -7,27 +7,45 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
+<link rel="stylesheet" href="${initParam.staticPath }/css/color.css" type="text/css">
+<!-- 
+<link href="${initParam.staticPath }/fileuploader/fileuploader.css" rel="stylesheet" type="text/css">
+ -->
+<script src="${initParam.staticPath }/fileuploader/fileuploader.js" type="text/javascript"></script>
+<script>        
+function createUploader(){            
+    var uploader = new qq.FileUploader({
+    	debug: true,
+        element: document.getElementById('file-uploader'),
+        action: 'upload',
+        uploadButtonText: '<span>Upload your photo now</span>',
+        template: '<div class="qq-uploader">' + '<div class="qq-upload-button upload button orange">{uploadButtonText}</div>' + '<ul class="qq-upload-list"></ul>' + '</div>',
+        extraDropzones: [qq.getByClass(document, 'qq-upload-extra-drop-area')[0]],
+        hideDropzones: false,
+        disableDefaultDropzone: true
+    });           
+}
+
+// in your app create uploader as soon as the DOM is ready
+// don't wait for the window to load  
+window.onload = createUploader;     
+</script>
 </head>
 <body>
 	<div class="grid-16 would-you-ctn">
 		<div class="would-you">
-			<div class="polaroid her"></div>
+			<div class="polaroid her qq-upload-extra-drop-area"></div>
 			<div class="details">
 				<h1>
 					<span>Would you contact this person?</span>
 				</h1>
 				<p>No? Well this is how you currently appear in search results.
 					Upload your photo and get 10x more views from possible matches.</p>
-				<div id="file-uploader">
-					<div class="qq-uploader">
-						<div style="position: relative; overflow: hidden; direction: ltr;"
-							class="qq-upload-button upload button orange">
-							<span>Upload your photo now</span><input
-								style="position: absolute; right: 0px; top: 0px; font-family: Arial; font-size: 118px; margin: 0px; padding: 0px; cursor: pointer; opacity: 0;"
-								name="file" multiple="multiple" type="file">
-						</div>
-						<ul class="qq-upload-list"></ul>
-					</div>
+				<div id="file-uploader">		
+					<noscript>			
+						<p>Please enable JavaScript to use file uploader.</p>
+						<!-- or put a simple form for upload here -->
+					</noscript>         
 				</div>
 				<p class="terms">All photos are moderated and checked before
 					they are published on the site. Please do not post pornographic,
@@ -40,6 +58,6 @@
 		</div>
 		<div class="clear"></div>
 		<div class="footer"></div>
-	</div>
+	</div>	
 </body>
 </html>
