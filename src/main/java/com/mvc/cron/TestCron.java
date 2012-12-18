9486b89c0f7e07aa76bee07da169b69abf6ef742
bennetgit <bennetchen22@gmail.com>
2012-12-18 14:37:01 +0800

    quartz cron 配置文件语法注释

--- src/main/java/com/mvc/cron/TestCron.java
+++ src/main/java/com/mvc/cron/TestCron.java
@@ -1,11 +1,15 @@
 package com.mvc.cron;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.apache.log4j.Logger;
 
 public class TestCron {
 	private Logger logger = Logger.getLogger(TestCron.class.getName());
 	
 	public void doBiz() {
-		logger.info("------------ Do Cron task ---------- ");
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+		logger.info("------------ Do Cron task: " + df.format(new Date()) + " ---------- ");
 	}
 }
